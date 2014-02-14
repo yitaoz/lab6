1ec9cb5c3545c40af12460a44b3e11affaff4a13
Yitao Zhang <yitaoz@stanford.edu>
2014-02-14 05:27:45 -0800

    done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -14,6 +14,9 @@ function initializePage() {
 	$('#colorBtn').click(randomizeColors);
 }
 
+
+
+
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
@@ -27,12 +30,38 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	var url = "/project/"+idNumber
+
+	$.get(url, function (result) {
+	console.log(url);
+	var project = "#project" + idNumber;
+
+	console.log(result);
+		var projecthtml = '<h1>' + result['title']+'</h1>';
+		projecthtml += '<h6>'+ result['date'] + '</h6>';
+		projecthtml += '<img src="'+ result['image'] +'" class="img">';
+		projecthtml += result['summary'];
+		$(project+" .details").html(projecthtml);
+		$(project+" .details .img").addClass('detailsImage');
+	});
 }
+
+	
+
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+	//console.log("User clicked on color button");
+	$.get("/palette", function(result) {
+		var colors = result['colors']['hex'];
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
 }
